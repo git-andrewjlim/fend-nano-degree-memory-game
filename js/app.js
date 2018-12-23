92a4dec8a04ae71616a242223cdf57ad5ea62062
Andrew <andrewjlim@icloud.com>
2018-12-23 22:57:08 +1100

    refractored - need to update layout(css) and put better card flip in

--- js/app.js
+++ js/app.js
@@ -1,69 +1,117 @@
-/* --- GLOBAL VARIABLES --- */
-const CARDS = 16;
+// ===============
+// GLOBAL VARIABLES
+// ===============
+
 const CARD_PAIRS = ['diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube', 'leaf', 'bicycle', 'bomb'];
-let STARS = 3;
+const CARDS = CARD_PAIRS.length * 2;
 const STAR_REDUCTION = CARD_PAIRS.length + 1;
-const docFragment = document.createDocumentFragment();
-const fontawesomePrefix = 'fa-';
+const DOCUMENT_FRAGMENT = document.createDocumentFragment();
+const FONTAWESOME_PREFIX = 'fa-';
+const STARS = 3;
 
+let stars = STARS;
 let turns = 0; // track how many guesses the user has made.
-let cardsMatched = 0; // track how many cards have been matched - if it equals CARDS then the game is over.
-let cardSelected = false; // track how many cards the user has chosen per turn (max: 2).
+let cardsMatched = 0; // track how many cards have been matched - if it equals CARDS then the game is over
+let cardSelected = false; // track how many cards the user has chosen per turn (max: 2)
 let timer = 0; // timer in seconds
-let resetButtonActive = false; // sets state of reset button eventListener
-let starCount = STARS; // tracks how many stars the user has.
-let checkEndGame = false; // track if the game should end.
-let deck = [];
-let card1, card2;
-let timerStart;
+let deck = []; // initialise the deck
+let card1, card2; // intitialise cards
+let timerStart; //initialise timer
 
-let timerLocation = document.querySelector('.timer');
-let modalPanel = document.querySelector('.modal');
+// Get DOM elements
+let $timer = document.querySelector('.timer'),
+    $modal = document.querySelector('.modal'),
+    $deck = document.querySelector('.deck'),
+    $stars = document.querySelector('.stars'),
+    $moves = document.querySelector('.moves'),
+    $startBtn = document.querySelector('.start-button'),
+    $restartBtn = document.querySelector('.restart-button'),
+    $replayBtn = document.querySelector('.replay-button'),
+    $modalStartContent = document.querySelector('.modal-inner-start'),
+    $modalEndContent = document.querySelector('.modal-inner-end');
 
-// Set up board and initialise values
+
+
+
+// ===============
+// INITIALISE
+// ===============
+
+
+initialise();
+
+
+
+
+// ===============
+// FUNCTIONS
+// ===============
+
+
+// INITIALISE THE PROGRAM
 function initialise() {
-    // check that CARD_PAIRS is half of CARDS (if not then throw error)
-    if (CARD_PAIRS.length * 2 === CARDS){
-        //setStars();
-        deck = fillDeck();
-        deck = shuffle(deck);
-        dealCards(deck);
-        resetTimer();
-        resetTurns();
-        
+    deck = fillDeck();
+    deck = shuffle(deck);
+    buildBoard();
+}
 
 
-    } else {
-        console.error('The number of card pairs is incorrect. There must be two of each kind');
+// BUILD THE BOARD
+function buildBoard() {
+    clearBoard();
+    dealCards(deck);
+}
+
+
+// RESET THE GAME
+function clearBoard() {
+    let cardDeck = $deck;
+
+    cardsMatched = 0;
+    clearInterval(timerStart);
+    resetTimer();
+    resetTurns();
+    resetStars();
+
+    // clear the cards   
+    while(cardDeck.firstChild) {
+        cardDeck.removeChild(cardDeck.firstChild);
     }
 }
 
-// Build the star value based upon constant STARS value
-function setStars() {
 
-        // grab the ul with .stars
-        
-        let starList = document.querySelector('.stars');
+// START THE GAME
+function startGame() { 
+    advanceTimer();
+    deck = shuffle(deck);
+    dealCards(deck);
+}
+
+// SET THE NUMBER OF STARS
+function setStars() {
+        let starList = $stars;
         let starItem = '<li><i class="fa fa-star"></i></li>';
+
         if (starList.childNodes.length == 0) {
-            // for the numer of STARS value add <li><i class="fa fa-star"></i></li>            
-            for(let i=0; i<STARS; i++) {
+            // for the numer of stars value add <li><i class="fa fa-star"></i></li>            
+            for(let i=0; i<stars; i++) {
                 let starItem = document.createElement('li');
                 starItem.innerHTML = '<i class="fa fa-star"></i>';
                 starList.appendChild(starItem);
             }
         } else {
-            for(let i=0;i<STARS; i++){
+            for(let i=0;i<stars; i++){
                 starList.childNodes[i].innerHTML = '<i class="fa fa-star"></i>';
             }
         }
 }
 
 
-// build the deck and return the deck
+// POPULATE THE DECK
 function fillDeck() {
     let arr_fillDeck = [];
-    // Create an array and put in each value twice // can use CARDS as the count
+
+    // Create an array and put in each value twice
     for(let i=0; i<CARD_PAIRS.length; i++) {
         arr_fillDeck.push(CARD_PAIRS[i]);
         arr_fillDeck.push(CARD_PAIRS[i]);
@@ -71,8 +119,7 @@ function fillDeck() {
     return arr_fillDeck;
 }
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-// may need to change this to let shuffledCards = (array) = > {}
+// SHUFFLE CARDS (http://stackoverflow.com/a/2450976)
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -87,128 +134,94 @@ function shuffle(array) {
     return array;
 }
 
-// build the deck of cards depending up on how many cards there are
+// ADD THE CARDS TO THE DOM
 function dealCards(deck) {
-    // @todo check if cards exist in DOM
-        // @todo if they do - clean board (remove items)
-    // build board
-        
-        // get the ul with .deck
-    let cardDeck = document.querySelector('.deck');
+    let cardDeck = $deck;
+
     if (cardDeck.childNodes.length == 0) {
-        // for each value within the deck add the following:
         for(let card of deck) {
             let cardItem = document.createElement('li');
             cardItem.setAttribute('class', 'card');
             cardItem.innerHTML = `<i class="fa fa-${card}"></i>`;
-            docFragment.appendChild(cardItem);
+            DOCUMENT_FRAGMENT.appendChild(cardItem);
         }
-        cardDeck.appendChild(docFragment);
-        // add eventListener for ul .cards
+
+        cardDeck.appendChild(DOCUMENT_FRAGMENT);
         cardDeck.addEventListener('click', selectCard);
-        // add a value to each class by getting the deck and adding 'fa-' to the front of it.
     } 
 }
 
-function clearBoard() {
-    //clear the cards
-    let cardDeck = document.querySelector('.deck');
-    while(cardDeck.firstChild) {
-        cardDeck.removeChild(cardDeck.firstChild);
-    }
-}
 
-// reset the timer to 0
+// RESET THE TIMER
 function resetTimer() {
     timer = 0;
 }
 
-// reset the turns
+
+// RESET TURNS
 function resetTurns() {
     turns = 0;
     updateTurns(turns);
 }
 
+
+//RESET STARS
 function resetStars() {
-    STARS = 3;
+    stars = STARS;
     setStars();
 }
 
+
+// INCREMENT OR RESET TURNS
 function updateTurns(turnNumber) {
-    let moves = document.querySelector('.moves');
+    let moves = $moves;
     moves.textContent = `${turnNumber}`;
 }
 
 
+// START TIMER
 function advanceTimer() {
-    // use set time out to increment by 1 every second
     timerStart = window.setInterval(function(){
         timer++;
-        timerLocation.textContent = `${timer}`;
+        $timer.textContent = `${timer}`;
     }, 1000);
 }
 
-// reduce stars depending on how many turns the player had
+
+// CHANGE STARS TO BLANKS STARS
 function reduceStars() {
-    // get the last child of ul.stars
     let starList = document.querySelectorAll('.fa-star');
-    if(turns % STAR_REDUCTION === 0 && STARS!== 0) {
-        
-        
-        if (STARS !== 0){ 
+
+    if(turns % STAR_REDUCTION === 0 && stars!== 0) {
+        if (stars !== 0){ 
             let starLost = starList[starList.length-1];
             starLost.setAttribute('class', 'fa fa-star-o');
-            STARS--;           
+            stars--;           
         }
-        console.log('reduce stars');
-        console.log(STARS);
-        // let starList = document.querySelectorAll('.fa-star');
-        // if (starList.length !== 0) {
-        // let starLost = starList[starList.length-1];
-        // starLost.setAttribute('class', 'fa fa-star-o');
-        // }
     }
 }
 
-function startGame() {
-    clearInterval(timerStart);
-    resetTimer();
-    resetTurns();
-    advanceTimer();
-    resetStars();
-    deck = shuffle(deck);
-    clearBoard();
-    dealCards(deck);
-}
 
-// show Congratulations pop up and ask if they would like to play again.
+// DISPLAY ENDING MESSAGE AND ALLOW FOR REPLAY
 function endGame() {
     clearInterval(timerStart);
     window.setTimeout(function(){
-    modalPanel.querySelector('.modal-inner').innerHTML = 
-        `<h1>Congratulations!</h1>
-        <p>You won the game</p>
-        <p>Your time was ${timer} seconds</p>
-        <p>You completed the game in ${turns} and earned ${STARS} star${STARS==1? '':'s'}</p>
-        <p><button class="play-again">Play Again</button></p>`;
-    modalPanel.style.cssText = 'display: block';
-    document.querySelector('.play-again').addEventListener('click', function(){
-        modalPanel.style.cssText = 'display: none';
-        startGame();
-            // initialise
-            // remove endGame panel
-            // checkEndGame = false;
-            // startTimer();
-            // enable reset button
-    });
+        $modalEndContent.querySelector('.completion-text').innerHTML = 
+        `<p>Your time was ${timer} seconds</p>
+        <p>You completed the game in ${turns} moves and earned ${stars} star${stars==1? '':'s'}</p>`;
+        $modal.style.cssText = 'display: block';
+        $modalStartContent.style.cssText = 'display: none';
+        $modalEndContent.style.cssText = 'display: block';
+        $replayBtn.addEventListener('click', function(){
+            $modal.style.cssText = 'display: none';
+            buildBoard();
+            startGame();
+        });
     }, 1000);
-    
-    //show popup with time, stars and congratulations!
-    //disable restart button
-    //show play again button
 }
 
-//flip card and nominate direction (optional)
+
+// FLIP THE SELECTED CARD
 function flipCard(card, flipDirection) {
     if(flipDirection != 'reverse') {
         card.setAttribute('class', 'card open show');
@@ -216,37 +229,28 @@ function flipCard(card, flipDirection) {
         card.setAttribute('class', 'card');
     }
 
-    
-    // direction it can be forwards (blank) or reverse
-    // get card selected
-    // check direction
-        //if reverse
-            //add close to class
-                // in css this:
-                    //minimises card face
-                    //changes opacity to 0
-                    //changes card back opacity to 100
-                    //expands card back
-                    //NOTE: Want to do it this way because in future may want to use an image instead of a card
-        //if no direction (default)
-            //add open to class
-                // in css this:
-                    //minimises card back
-                    //changes opacity to 0
-                    //changes card face opacity to 100
-                    //expands card face
+    // @TODO FLIPPING MECHANICS
+        //minimises card face
+        //changes opacity to 0
+        //changes card back opacity to 100
+        //expands card back
+        //NOTE: Want to do it this way because in future may want to use an image instead of a card
 }
 
+
+// RETURN ICON OF CARD
 function getIcon(cardIcon) {
     cardIcon = cardIcon.querySelector('.fa');
     let cardIconClasses = cardIcon.getAttribute('class').split(' ');
     for(cardIconClass of cardIconClasses) {
-        if(cardIconClass.match(fontawesomePrefix)) {
+        if(cardIconClass.match(FONTAWESOME_PREFIX)) {
             return cardIconClass;
         }               
     }
 }
 
+
+// CHECK WHETHER CARDS ARE A MATCH
 function checkMatch(card1, card2) {
     let card1Icon, card2Icon;
     let match;
@@ -262,36 +266,10 @@ function checkMatch(card1, card2) {
     return match;
 }
 
-
-initialise();
-
-
-/* --- EVENT LISTENERS --- */
-// start button
-document.querySelector('.start-button').addEventListener('click', function(){
-    // hide instructions
-    modalPanel.style.cssText = 'display: none;';
-    startGame();
-    // delete start button (you wont use it again)
-    // startTimer();
-    // enable reset button
-});
-
-
-// reset button
-document.querySelector('.restart').addEventListener('click', function(){
-    startGame();
-    // initialise
-    // startTimer();
-    // enable reset button
-});
-
-// play again (same as reset button)
-
-
+// CONTROL CARD EVENT HANDLER
 function selectCard(e) {
     let cardNode = e.target;
-    let match, gameEnded;
+    let match;
 
     // only activate if card is clicked (not the deck)
     if (cardNode.getAttribute('class') === 'card') {
@@ -321,96 +299,35 @@ function selectCard(e) {
                     cardSelected = false;
                 cardNode.parentNode.addEventListener('click', selectCard);
                 }, 500);
-            }
-            
-            
+            }            
         }
     }
-
-
-    
-    
-    
-    
-    
-    
-    
-
-
-    // console.log(cardIcon);
-
-    // // if cardSelected = false;(1st choice) - allow another card to be flipped
-    // if (cardSelected === false) {
-    //         // flipcard(this)
-    //         // set show on card (this is what you will search against)
-    //         // set cardSelected = true
-    // } else {
-    // if cardSelected = true; (2nd choice)
-            //disable eventListeners on cards
-            // check classes for show (this should be the other)
-            // get its icon class
-            //check if icon class is a match
-                //if it is a match
-                    //change card class of other card from show to match
-                    //add match to this cards class
-                    // add 1 to cardsMatched
-                    // check if cardsMatched = CARDS
-                        // checkEndGame = true
-                //if it is not a match        
-                    //remove show on other card
-                    // flipCards(this, 'reverse')
-                    // reinstate eventListeners on card
-//     }
-//     // check if STAR_REDUCTION === turns
-//         // if true - reduceStars()
-//     // check if checkEndGame = true;
-//         // if true - endGame() 
-}  
-    
-
-
-
-
-
-
-
-/* --- INSTRUCTIONS GIVEN BY UDACITY --- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-/*function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
 }
 
 
- * Create a list that holds all of your cards
- */
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// ===============
+// EVENT LISTENERS
+// ===============
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// START BUTTON
+$startBtn.addEventListener('click', function(){
+    // hide instructions
+    $modal.style.cssText = 'display: none;';
+    startGame();
+    // delete start button (you wont use it again)
+    // startTimer();
+    // enable reset button
+});
+
+
+// RESET BUTTON
+$restartBtn.addEventListener('click', function(){
+    buildBoard();
+    startGame();
+    // initialise
+    // startTimer();
+    // enable reset button
+});
